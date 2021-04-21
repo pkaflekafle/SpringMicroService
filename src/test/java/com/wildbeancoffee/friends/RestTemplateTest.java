4120bb43be4e7944b5f3cfe1c1bfab7bf782c179
pkafle <pkafle@gmail.com>
2021-04-20 20:45:08 -0400

    springmicroservice code

---
+++ src/test/java/com/wildbeancoffee/friends/RestTemplateTest.java
@@ -0,0 +1,27 @@
+package com.wildbeancoffee.friends;
+
+import com.wildbeancoffee.friends.controllers.FriendController;
+import com.wildbeancoffee.friends.model.Friend;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestTemplateTest{
+
+  TestRestTemplate restTemplate = new TestRestTemplate();
+
+  @Test
+  public void testTemplate() {
+
+
+  }
+
+
+}
